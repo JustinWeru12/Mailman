7558a8f969d17ab040700fad62e86271f49ca91d
justinweru12 <justinivor96@gmail.com>
2019-10-07 12:17:33 +0300

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './services/user/auth.guard';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule), canActivate: [AuthGuard],},
+  { path: 'event-create', loadChildren: './pages/event-create/event-create.module#EventCreatePageModule',canActivate: [AuthGuard], },
+  { path: 'event-list', loadChildren: './pages/event-list/event-list.module#EventListPageModule' },
+  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
+  { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
+  { path: 'event-detail/:id', loadChildren: './pages/event-detail/event-detail.module#EventDetailPageModule',canActivate: [AuthGuard], },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
+  { path: 'tracker', loadChildren: './tracker/tracker.module#TrackerPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/credentials.ts
@@ -0,0 +1,10 @@
+//initialize Firebase
+export const firebaseConfig = {
+    apiKey: "AIzaSyBLs8r5zSOBO9mi4I896OxGDbVQPa3FBo0",
+    authDomain: "mailman-3e739.firebaseapp.com",
+    databaseURL: "https://mailman-3e739.firebaseio.com",
+    projectId: "mailman-3e739",
+    storageBucket: "mailman-3e739.appspot.com",
+    messagingSenderId: "60761941589",
+    appId: "1:60761941589:web:676d3d322a88c5bd"
+  };
